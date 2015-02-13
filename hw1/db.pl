040fd6891b451152fc2b52ff579afb8da9472c30
mayc2 <mayc2@rpi.edu>
2015-02-13 16:24:31 -0500

    got rid of Singleton variables error, but not sure how to not write and use Tree

--- hw1/db.pl
+++ hw1/db.pl
@@ -5,6 +5,7 @@
 
 %sentence end terminator
 end --> ['.'].
+q_end --> ['?'].
 
 %Determiners
 det(det(a)) --> [a].
@@ -23,6 +24,16 @@ verb(verb(stayed)) --> [stayed].
 verb(verb(flew)) --> [flew].
 verb(verb(arrived)) --> [arrived].
 
+%Present Verbs
+pres_verb(pres_verb(leave)) --> [leave].
+pres_verb(pres_verb(fly)) --> [fly].
+pres_verb(pres_verb(arrive)) --> [arrive].
+pres_verb(pres_verb(stay)) --> [stay].
+
+%Fillers -- "did not"
+f1(f1(did)) --> [did].
+f2(f2(not)) --> [not].
+
 %Nominal
 nom(nom(Word)) --> noun(Word).
 
@@ -33,7 +44,29 @@ vp(vp(Word)) --> verb(Word).
 np(np(Word1,Word2)) --> det(Word1), nom(Word2).
 
 %sentence, exepects a noun phrase and verb phrase
-s(s(S,V)) --> np(S), vp(V), end.
+s(s(S,V)) --> np(np(S)), verb(verb(V)), end, convert(V,Nv), assert(mem(S,Nv)).
+
+%convert past to present for easier comparison
+convert(convert(left,N)) --> N = leave.
+convert(convert(stayed,N)) --> N = fly.
+convert(convert(flew,N)) --> N = arrive.
+convert(convert(arrived,N)) --> N = stay.
+
+%contra-sentence
+cs(cs(S,V)) --> [did], np(np(S)), pres_verb(pres_verb(V)), q_end, solve(S,V,B), output(B).
+
+%solve, and return yes ot no
+solve(S,V,Bool):-
+	mem(S,V), Bool is 1. 
+solve(S,V,Bool):-
+	Bool is 0,mem(S,V).
+
+%output
+output(0):- write(no).
+output(1):- write(yes).	
+
+%Check enables us to check for question or statement sentence
+check(check(Tree)) --> s(Tree) | cs(Tree).
 
 %Loop that parses sentences
 loop:-
@@ -45,5 +78,4 @@ loop:-
 
 %parse predicate
 parse(Tree,Sentence):-
-	phrase(s(Tree),Sentence).
-
+	phrase(check(Tree),Sentence).
